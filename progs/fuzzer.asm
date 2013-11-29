a15c65a0b7280f7fd42848fadaef91417f6b8917
Sean Cross <xobs@kosagi.com>
2013-11-29 11:46:15 +0800

    fuzz: Increase search space for unusual pin changes
    We now search 64 bytes for changes.  Additionally, we will flip up
    to 8 SFRs in one go, rather than limiting it to 5 SFRs.

--- progs/fuzzer.asm
+++ progs/fuzzer.asm
@@ -35,9 +35,12 @@ start:
         .db     0xa5, 0x02, 0x02
         .db     0xa5, 0x03, 0x03
         .db     0xa5, 0x04, 0x04
-        mov     R2, #0x00
-        mov     R3, #0
-        mov     R4, #0x02
+        .db     0xa5, 0x05, 0x05
+        .db     0xa5, 0x06, 0x06
+        .db     0xa5, 0x07, 0x07
+        mov     R2, #0
+        mov     R3, #3
+        mov     R4, #0
 top_of_pause1:
         djnz    R2, top_of_pause1
         djnz    R3, top_of_pause1
@@ -47,9 +50,12 @@ top_of_pause1:
         .db     0xa5, 0x82, 0x82
         .db     0xa5, 0x83, 0x83
         .db     0xa5, 0x84, 0x84
-        mov     R2, #0x00
-        mov     R3, #0
-        mov     R4, #0x02
+        .db     0xa5, 0x85, 0x85
+        .db     0xa5, 0x86, 0x86
+        .db     0xa5, 0x87, 0x87
+        mov     R2, #0
+        mov     R3, #3
+        mov     R4, #0
 top_of_pause2:
         djnz    R2, top_of_pause2
         djnz    R3, top_of_pause2
--- src/ax215/fuzz.c
+++ src/ax215/fuzz.c
@@ -1,75 +1,27 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <arpa/inet.h>
 #include <strings.h>
 #include <string.h>
 #include <time.h>
 #include <errno.h>
-#include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include "sd.h"
 
-/** Definitions for Novena EIM interface */
-#define CS_PIN    GPIO_IS_EIM | 3
-#define MISO_PIN  GPIO_IS_EIM | 0
-#define CLK_PIN   GPIO_IS_EIM | 4
-#define MOSI_PIN  GPIO_IS_EIM | 5
-#define DAT1_PIN  GPIO_IS_EIM | 1
-#define DAT2_PIN  GPIO_IS_EIM | 2
-#define POWER_PIN 17 //GPIO1_IO17
 
-
-// R1 Response Codes (from SD Card Product Manual v1.9 section 5.2.3.1)
-#define R1_IN_IDLE_STATE    (1<<0)  // The card is in idle state and running initializing process.
-#define R1_ERASE_RESET      (1<<1)  // An erase sequence was cleared before executing because of an out of erase sequence command was received.
-#define R1_ILLEGAL_COMMAND  (1<<2)  // An illegal command code was detected
-#define R1_COM_CRC_ERROR    (1<<3)  // The CRC check of the last command failed.
-#define R1_ERASE_SEQ_ERROR  (1<<4)  // An error in the sequence of erase commands occured.
-#define R1_ADDRESS_ERROR    (1<<5)  // A misaligned address, which did not match the block length was used in the command.
-#define R1_PARAMETER        (1<<6)  // The command's argument (e.g. address, block length) was out of the allowed range for this card.
-
-static int interesting_one_cycle(struct sd_state *state, int run, int seed) {
-    uint8_t response[16];
-    static int spots = 5;
+static int patch_fuzzer(struct sd_state *state, uint8_t *file, int filesize) {
+    static int spots = 8;
     uint8_t sfrs[spots];
     uint8_t sfrs_val0[spots];
     uint8_t sfrs_val1[spots];
-    uint8_t file[512+(4*sizeof(uint16_t))];
-	int i = 0;
-    int ret;
-
-    memset(response, 0, sizeof(response));
-    memset(file, 0, sizeof(file));
-
-    srand(seed);
-    printf("\n\nRun %-4d  Seed: %8d\n", run, seed);
-
-    memset(file, 0, sizeof(file));
-    int fd = open("fuzzer.bin", O_RDONLY);
-    if (-1 == fd) {
-        perror("Couldn't open binary program");
-        exit(1);
-    }
-    read(fd, file, 512);
-    close(fd);
+    int i;
 
     // Patch the program binary
-    for (i=0; i<spots; i++) {
+    for (i = 0; i < spots; i++) {
         sfrs[i] = (rand()&0x7f) | 0x80;
-//        if (sfrs[i] == 0xef || sfrs[i] == 0xf2)
-//            continue;
-
         sfrs_val0[i] = rand();
         sfrs_val1[i] = rand();
-    }
-
-    // Actually enter factory mode (sends CMD63/APPO and waits for response)
-    ret = sd_enter_factory_mode(state, run);
-    if (-1 == ret) {
-        printf("Couldn't enter factory mode\n");
-        return -1;
     }
 
     int matched = 0;
@@ -92,6 +44,7 @@ static int interesting_one_cycle(struct sd_state *state, int run, int seed) {
                     file[i+2] = sfrs_val0[reg];   // Immediate value
             }
             else {
+		/* Fill with NOPs */
                 file[i+0] = 0;
                 file[i+1] = 0;
                 file[i+2] = 0;
@@ -105,18 +58,53 @@ static int interesting_one_cycle(struct sd_state *state, int run, int seed) {
         else {
             printf("Error locating special opcode (%d %02x %02x / %02x)\n",
                     i, file[i+1], file[i+2], file[i+1]&0x7f);
-            exit(0);
+            return -1;
         }
     }
 
     if (matched != spots*2) {
         printf("Couldn't find %d matches, only found %d\n", spots*2, matched);
-        exit(1);
+        return -2;
     }
 
-    fd = open("fuzzer-out.bin", O_WRONLY | O_CREAT | O_TRUNC, 0777);
-    write(fd, file, 512);
+    for (i=0; i<total; i++)
+        printf("    SFR_%02x:  %02x / %02x\n", sfrs[i], sfrs_val0[i], sfrs_val1[i]);
+
+    return 0;
+}
+
+static int interesting_one_cycle(struct sd_state *state, int run, int seed) {
+    uint8_t response[16];
+    uint8_t slow_response[64];
+    uint8_t file[512+(4*sizeof(uint16_t))];
+    int i = 0;
+    int ret;
+
+    memset(response, 0, sizeof(response));
+    memset(file, 0, sizeof(file));
+
+    srand(seed);
+    printf("\nRun %-4d  Seed: %8d\n", run, seed);
+
+    memset(file, 0, sizeof(file));
+    int fd = open("fuzzer.bin", O_RDONLY);
+    if (-1 == fd) {
+        perror("Couldn't open binary program");
+        exit(1);
+    }
+    read(fd, file, 512);
     close(fd);
+
+    ret = patch_fuzzer(state, file, sizeof(file));
+    if (ret < 0)
+	    return ret;
+
+    // Actually enter factory mode (sends CMD63/APPO and waits for response)
+    ret = sd_enter_factory_mode(state, run);
+    if (-1 == ret) {
+        printf("Couldn't enter factory mode\n");
+        return ret;
+    }
 
     // Transmit the file
     sd_mmc_dat4_crc16(file, file+(sizeof(file)-8), sizeof(file)-8);
@@ -126,27 +114,26 @@ static int interesting_one_cycle(struct sd_state *state, int run, int seed) {
 
     // Wait for some sign of life
     int sd_pins = sd_read_pins(state);
-    int matches = 0;
-    int sleeptime = 100*(rand()&0xf);
-    for (i=0; i<10; i++) {
+    int changes = 0;
+    int sleeptime = 10 *(1 + (rand() & 0xff));
+    for (i = 0; i < sizeof(slow_response); i++) {
         usleep(sleeptime);
-        int new_pins = sd_read_pins(state);
-        if (new_pins != sd_pins) {
-            matches++;
-            sd_pins = new_pins;
+        slow_response[i] = sd_read_pins(state);
+        if (slow_response[i] != sd_pins) {
+            changes++;
         }
+        sd_pins = slow_response[i];
     }
 
     printf("\nResponse:\n");
     print_hex(response, sizeof(response));
-    for (i=0; i<spots; i++)
-        printf("SFR_%02x:  %02x / %02x\n", sfrs[i], sfrs_val0[i], sfrs_val1[i]);
-    printf("%d pin matches (last: %x)\n", matches, sd_pins);
+    printf("%d state changes, slow response:\n", changes);
+    print_hex(slow_response, sizeof(slow_response));
     printf("Finished up run: %-4d  Seed: %8d\n", run, seed);
-    printf("--------------------------------------------------\n");
+    printf("\n==================================================\n");
 
 
-    if (matches >= 5) {
+    if (changes >= 5) {
         printf("Potential match\n");
         return 1;
     }
