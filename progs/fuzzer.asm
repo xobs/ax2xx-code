581dc727e5d7dd4b3f169ded80d5e5896656948e
Sean Cross <xobs@kosagi.com>
2013-12-02 14:43:28 +0800

    fuzzer: Remove AX211 macro definitions
    The macro definitions are exclusive to the AX211.  We're on the AX215 now,
    so remove the definitions in preparation for coming up with new ones.

--- progs/fuzzer.asm
+++ progs/fuzzer.asm
@@ -1,31 +1,3 @@
-.equ    IEN0, 0xA8
-.equ    SDICMD, 0x8D
-
-.equ    SDOSTATE, 0x98
-
-.equ    SDOADDRL, 0x96
-.equ    SDOADDRH, 0x97
-
-.equ    SDOBYTESL, 0x93
-.equ    SDOBYTESH, 0x94
-
-.equ    SDODMAADDRL, 0x99
-.equ    SDODMAADDRH, 0x9A
-
-.equ    SDODMABYTESL, 0x91
-.equ    SDODMABYTESH, 0x92
-
-.equ    SDOS, 0x88
-.equ    SDDIR, 0xEB
-.equ    FSR_80, 0x80
-.equ    FSR_8E, 0x8E
-.equ    FSR_90, 0x90
-.equ    FSR_D8, 0xD8
-.equ    FSR_EB, 0xEB
-.equ    FSR_FC, 0xFC
-
-.equ    RESET, 0
-
 .org 0x7b00
 
 ; This gets called from an interrupt.
@@ -45,6 +17,8 @@ top_of_pause1:
         djnz    R2, top_of_pause1
         djnz    R3, top_of_pause1
         djnz    R4, top_of_pause1
+
+anti_start:
         .db     0xa5, 0x80, 0x80
         .db     0xa5, 0x81, 0x81
         .db     0xa5, 0x82, 0x82
