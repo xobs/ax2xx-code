e37bdc294963231171352b2984f00d084276a5b8
Sean Cross <xobs@kosagi.com>
2013-12-06 16:54:59 +0800

    Revert "fpga-config: Have shell script call sudo"
    This reverts commit 194a7924a5d28be791f76aa7b86a07d8ae537479.

--- fpga/fpga-config.sh
+++ fpga/fpga-config.sh
@@ -8,16 +8,16 @@ fi
 bitfile=$1
 
 echo "Setting export of reset pin"
-echo 135 | sudo tee /sys/class/gpio/export 2> /dev/null
+echo 135 > /sys/class/gpio/export 2> /dev/null
 echo "setting reset pin to out"
-echo out | sudo tee /sys/class/gpio/gpio135/direction 2> /dev/null
+echo out > /sys/class/gpio/gpio135/direction 2> /dev/null
 echo "flipping reset"                       
-echo 0 | sudo tee /sys/class/gpio/gpio135/value 2> /dev/null
-echo 1  /sys/class/gpio/gpio135/value 2> /dev/null
+echo 0 > /sys/class/gpio/gpio135/value 2> /dev/null
+echo 1 > /sys/class/gpio/gpio135/value 2> /dev/null
                                       
 echo "configuring FPGA"               
                                       
-sudo dd if=${bitfile} of=/dev/spidev2.0 bs=128
+dd if=${bitfile} of=/dev/spidev2.0 bs=128
                                                              
 echo "turning on clock to FPGA"                              
-sudo devmem2 0x020c8160 w 0x00000D2B
+devmem2 0x020c8160 w 0x00000D2B
